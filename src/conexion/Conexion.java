9f1c5b667dd5a9fbeb102ba7cdceb84b2c0aa65d
Xavier Yanza <xavier.y.2013@gmail.com>
2020-09-07 23:35:26 -0500

    Conexion

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -7,33 +7,62 @@ package conexion;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Asrock Gamer
  */
 public class Conexion {
+     Connection conexion ;
+     
+    public Conexion() {
+    }
+    
+    
 
-    public void connectDatabase() {
-        try {
-            // We register the PostgreSQL driver
-            // Registramos el driver de PostgresSQL
-            try { 
-                Class.forName("org.postgresql.Driver");
-            } catch (ClassNotFoundException ex) {
-                System.out.println("Error al registrar el driver de PostgreSQL: " + ex);
-            }
-            Connection connection = null;
-            // Database connect
-            // Conectamos con la base de datos
-            connection = DriverManager.getConnection(
-                    "jdbc:postgresql://192.168.0.26:5432/customerdb",
-                    "xulescode", "xulescode");
- 
-            boolean valid = connection.isValid(50000);
-            System.out.println(valid ? "TEST OK" : "TEST FAIL");
-        } catch (java.sql.SQLException sqle) {
-            System.out.println("Error: " + sqle);
-        }
-    } 
+//    public void connectDatabase() {
+//        try {
+//            // We register the PostgreSQL driver
+//            // Registramos el driver de PostgresSQL
+//            try { 
+//                Class.forName("org.postgresql.Driver");
+//            } catch (ClassNotFoundException ex) {
+//                System.out.println("Error al registrar el driver de PostgreSQL: " + ex);
+//            }
+//            Connection connection = null;
+//            // Database connect
+//            // Conectamos con la base de datos
+//            connection = DriverManager.getConnection(
+//                    "jdbc:postgresql://192.168.0.26:5432/customerdb",
+//                    "xulescode", "xulescode");
+// 
+//            boolean valid = connection.isValid(50000);
+//            System.out.println(valid ? "TEST OK" : "TEST FAIL");
+//        } catch (java.sql.SQLException sqle) {
+//            System.out.println("Error: " + sqle);
+//        }
+//    } 
+    
+    public Connection conexion(String usuario,String password)
+    {
+         
+           String url="jdbc:postgresql://database-1.cqky7cb50g2m.us-east-1.rds.amazonaws.com:5432/SistemaMatriculas";
+           
+           try{
+               Class.forName("org.postgresql.Driver");
+               conexion = DriverManager.getConnection(url,usuario,password);
+               if(conexion !=null)
+               {
+                   System.out.println("CONECTADO A LA BASE DE DATOS");
+               }      
+           }
+           catch(Exception e)
+           {
+              JOptionPane.showMessageDialog(null,"PROBLEMAS DE CONEXION "+e);
+           }
+           
+           return conexion;
+         
+    }     
 }
