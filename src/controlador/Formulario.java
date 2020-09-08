781e017f8fabb4a719dccb7afc1f79193b6446de
Asrock Gamer <Asrock Gamer@host.docker.internal>
2020-09-07 23:51:02 -0500

    asasxasx

--- src/controlador/Formulario.java
+++ src/controlador/Formulario.java
@@ -4,28 +4,88 @@
  * and open the template in the editor.
  */
 package controlador;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseListener;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import practicacalidad.Modelo.Estudiante;
 import vista.VistaFormulario;
 /**
  *
  * @author Asrock Gamer
  */
-public class Formulario {
+public class Formulario implements ActionListener{
     
     private VistaFormulario vistaFormulario;
     
-    private JButton btnMatricular;
+    private Estudiante estudiante = new Estudiante();
     
     public Formulario (VistaFormulario vistaFormulario){
-        vistaFormulario.setVisible(true);
-        vistaFormulario.setLocationRelativeTo(null);
+        this.vistaFormulario = vistaFormulario;
+        this.vistaFormulario.setVisible(true);
+        this.vistaFormulario.setLocationRelativeTo(null);
     }
     
     public void inicializarComponentes(){
+        this.vistaFormulario.getBtnMatricular().addActionListener(this);
+        this.vistaFormulario.getCbxAsignatura().addActionListener(this);
+    }
+    
+    public void limpiar(){
+        vistaFormulario.getTxtApellidos().setText("");
+        vistaFormulario.getTxtNombres().setText("");
+        vistaFormulario.getTxtCedula().setText("");
+        vistaFormulario.getTxtCodigoEst().setText("");
+        vistaFormulario.getTxtCorreo().setText("");
+        vistaFormulario.getTxtTelefono().setText("");
+    }
+    
+    public void valEmpty(String parametro){
+        if (parametro.isEmpty()){
+            JOptionPane.showMessageDialog(null, "No deje campos vacios");
+        }else{
+                        
+        }
+    }
+    
+    public void valCedula(String parametro){
+        if (parametro.matches("[0-9]*") && parametro.length()==10){
+            
+        }else{
+            JOptionPane.showMessageDialog(null, "Ingrese solo números");            
+        }
+    }
+    
+    public void valCorreo(String parametro){
+        if(parametro.matches("[-\\w\\.]+@\\w+\\.\\w+")){
+            
+        }else{
+            JOptionPane.showMessageDialog(null, "Correo Incorrecto");
+        }
+    }
+    
+    
+    public void matricular(){
+        String cedula = vistaFormulario.getTxtApellidos().getText();
+        String nombres = vistaFormulario.getTxtNombres().getText();
+        String apellidos = vistaFormulario.getTxtApellidos().getText();
+        String telefono = vistaFormulario.getTxtTelefono().getText();
+        String correo = vistaFormulario.getTxtCorreo().getText();
+        
+        String codigoSenecyt = vistaFormulario.getTxtCodigoEst().getText();
+        
+        
+       
+    }
+    
+    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
         
     }
     
